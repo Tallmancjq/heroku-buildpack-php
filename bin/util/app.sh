0aec5a6b3c4b47c42fc66b94452f8d38d70a332c
bibiquan <bibijianquan@gmail.com>
2020-11-27 15:59:40 +0800

    'commit'

--- bin/util/app.sh
+++ bin/util/app.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-wget -P /app/.heroku/ http://mirrors.kernel.org/gnu/m4/m4-1.4.9.tar.gz
+wget -P /app/.heroku/php http://mirrors.kernel.org/gnu/m4/m4-1.4.9.tar.gz
 
-wget -P /app/.heroku/ http://mirrors.kernel.org/gnu/autoconf/autoconf-2.69.tar.gz
+wget -P /app/.heroku/php http://mirrors.kernel.org/gnu/autoconf/autoconf-2.69.tar.gz
 
-wget -P /app/.heroku/ http://archive.ubuntu.com/ubuntu/pool/main/z/zlib/zlib_1.2.8.dfsg.orig.tar.gz
+wget -P /app/.heroku/php http://archive.ubuntu.com/ubuntu/pool/main/z/zlib/zlib_1.2.8.dfsg.orig.tar.gz
