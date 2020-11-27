2149569bf2400b2c70d7973b01737b1e180721ca
bibiquan <bibijianquan@gmail.com>
2020-11-27 15:37:44 +0800

    'commit'

---
+++ bin/util/app.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd /app/.heroku
+
+wget http://mirrors.kernel.org/gnu/m4/m4-1.4.9.tar.gz
+
+wget http://mirrors.kernel.org/gnu/autoconf/autoconf-2.69.tar.gz
+
+wget http://archive.ubuntu.com/ubuntu/pool/main/z/zlib/zlib_1.2.8.dfsg.orig.tar.gz
