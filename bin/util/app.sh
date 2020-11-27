a6135842fc53c5bd5e41af5d546de66d2b8b1ca6
bibiquan <bibijianquan@gmail.com>
2020-11-27 16:43:48 +0800

    'commit'

--- bin/util/app.sh
+++ bin/util/app.sh
@@ -17,6 +17,13 @@ make && make install
 cd /app/.heroku/php
 wget http://archive.ubuntu.com/ubuntu/pool/main/z/zlib/zlib_1.2.8.dfsg.orig.tar.gz
 tar -xzvf zlib_1.2.8.dfsg.orig
-cd zlib_1.2.8.dfsg.orig
+cd zlib_1.2.8
 ./configure --prefix=/app/.heroku/php
+make && make install
+
+cd /app/.heroku/php
+git clone https://github.com/viest/php-ext-excel-export
+cd php-ext-excel-export
+git submodule update --init
+phpize && ./configure --with-php-config=/app/.heroku/php/bin/php-config --enable-reader
 make && make install
